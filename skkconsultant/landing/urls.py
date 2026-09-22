139fcaaecafe6d703b5b21f5c1083e4e5a58b06a
unknown <solomonm014@gmail.com>
2026-09-22 14:26:21 +0300

    .

--- skkconsultant/landing/urls.py
+++ skkconsultant/landing/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = 'landing'
 
 urlpatterns = [
-    path('', Index.as_view(), name='landing-index'),
-    path('about/', About.as_view(), name='landing-about'),
-    path('contact/', Contact.as_view(), name='landing-contact'),
-    path('gallery/', Gallery.as_view(), name='landing-gallery'),
-    path('services/', Services.as_view(), name='landing-services'),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('contact/', Contact.as_view(), name='contact'),
+    path('gallery/', Gallery.as_view(), name='gallery'),
+    path('services/', Services.as_view(), name='services'),
 ]
