4d337831d302a7a82400ce891ede910af51ed15f
unknown <solomonm014@gmail.com>
2026-09-23 00:18:23 +0300

    .

--- skkconsultant/skkconsultant/wsgi.py
+++ skkconsultant/skkconsultant/wsgi.py
@@ -1,16 +1,22 @@
-"""
-WSGI config for skkconsultant project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skkconsultant.settings')
+sys.path.append('/var/www/skkconsultant/skkconsultant/skkconsultant')
+# adjust the Python version in the line below as needed
+sys.path.append('/var/www/skkconsultant/env/lib/python3.12/site-packages')
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skkconsultant.settings")
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
