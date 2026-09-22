4846a2ff10de45ee11df95007f5286e152128ca9
unknown <solomonm014@gmail.com>
2026-09-22 12:41:59 +0300

    .

--- skkconsultant/landing/urls.py
+++ skkconsultant/landing/urls.py
@@ -5,4 +5,8 @@ app_name = 'landing'
 
 urlpatterns = [
     path('', Index.as_view(), name='landing-index'),
+    path('about/', About.as_view(), name='landing-about'),
+    path('contact/', Contact.as_view(), name='landing-contact'),
+    path('gallery/', Gallery.as_view(), name='landing-gallery'),
+    path('services/', Services.as_view(), name='landing-services'),
 ]
--- skkconsultant/landing/views.py
+++ skkconsultant/landing/views.py
@@ -3,3 +3,19 @@ from accounts.auth_views import AnonymousView
 class Index(AnonymousView):
     def handle_get(self):
         self.html = 'landing/index.html'
+
+class About(AnonymousView):
+    def handle_get(self):
+        self.html = 'landing/about.html'
+
+class Gallery(AnonymousView):
+    def handle_get(self):
+        self.html = 'landing/gallery.html'
+
+class Contact(AnonymousView):
+    def handle_get(self):
+        self.html = 'landing/contact.html'
+
+class Services(AnonymousView):
+    def handle_get(self):
+        self.html = 'landing/services.html'
